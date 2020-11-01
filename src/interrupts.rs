f88e8d214ba8eddf7bd6c5b4ec59ada89c358237
Erik Badger <erikbadger777@gmail.com>
2020-11-01 10:44:58 -0600

    (feature) Added keyboard event handling

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -16,6 +16,7 @@ pub static PICS: spin::Mutex<ChainedPics> =
 #[repr(u8)]
 pub enum InteruptsIndex {
     Timer = PIC_1_OFFSET,
+    Keyboard,
 }
 
 impl InteruptsIndex {
@@ -37,8 +38,13 @@ lazy_static! {
         let mut idt = InterruptDescriptorTable::new();
 
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+
         idt[InteruptsIndex::Timer.as_usize()]
             .set_handler_fn(timer_interupt_handler);
+
+        idt[InteruptsIndex::Keyboard.as_usize()]
+            .set_handler_fn(keyboard_interupt_handler);
+
         unsafe {
             idt.double_fault.set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
@@ -62,6 +68,38 @@ extern "x86-interrupt" fn timer_interupt_handler (
     unsafe {
         PICS.lock()
             .notify_end_of_interrupt(InteruptsIndex::Timer.as_u8());
+    }
+}
+
+extern "x86-interrupt" fn keyboard_interupt_handler (
+    _stack_frames: &mut InterruptStackFrame
+) {
+    use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+    use spin::Mutex;
+    use x86_64::instructions::port::Port;
+
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> = {
+            Mutex::new(Keyboard::new(layouts::Us104Key, ScancodeSet1, HandleControl::Ignore))
+        };
+    }
+
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
+
+    let scancode: u8 = unsafe { port.read() };
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                DecodedKey::RawKey(key) => print!("{:?}", key),
+            }
+        }
+    }
+
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InteruptsIndex::Keyboard.as_u8());
     }
 }
 
